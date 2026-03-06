ddd5f6222b1410d05ce82b09fb759ebebab4a427
Samuel Ferreira <samuelferreiradasilva1570@gmail.com>
2026-03-05 21:49:35 -0300

    novo commit

---
+++ logica2_python.py
@@ -0,0 +1,12 @@
+# Solicitando dados do usuário
+
+nome = input('Digite seu nome: ')
+print(f'Seu nome é {nome}')
+print(type(nome))
+
+idade = int(input('Digite sua idade: '))
+
+# Ou então idade = int(idade) na linha abaixo
+
+print(f'Sua idade é {idade}')
+print(type(idade))
